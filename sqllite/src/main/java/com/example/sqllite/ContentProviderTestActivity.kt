51c5286ef70ec81b31d8ee2f4a671660b3cf8629
Xudoyshukur <Khudoyshukur.Juraev.001@mail.ru>
2022-01-14 17:28:38 +0500

    added asynchronous way of querying contentResolver

--- sqllite/src/main/java/com/example/sqllite/ContentProviderTestActivity.kt
+++ sqllite/src/main/java/com/example/sqllite/ContentProviderTestActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sqllite
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import com.example.sqllite.database.HoardContract
 import com.example.sqllite.databinding.ActivityContentProviderTestBinding
 import com.example.sqllite.entity.Hoard
@@ -24,35 +28,48 @@ class ContentProviderTestActivity : AppCompatActivity() {
         binding.recyclerView.adapter = hoardsAdapter
 
         binding.btnLoad.setOnClickListener {
-            val cursor = contentResolver.query(
-                MyHoardContentProvider.CONTENT_URI,
-                null, null, null, null,
+            LoaderManager.getInstance(this).initLoader(LOADER_ID, null, MyContentLoader())
+        }
+    }
+
+    private inner class MyContentLoader() : LoaderManager.LoaderCallbacks<Cursor> {
+        override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+            return CursorLoader(
+                this@ContentProviderTestActivity, MyHoardContentProvider.CONTENT_URI,
+                null, null, null, null
             )
-            cursor?.use {
-                if (cursor.columnCount > 0) {
-                    val hoards = arrayListOf<Hoard>()
-                    while (cursor.moveToNext()) {
-                        val indexId = cursor.getColumnIndex(HoardContract.KEY_ID)
-                        val indexName =
-                            cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARD_NAME_COLUMN)
-                        val indexGoldHoarded =
-                            cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARDED_COLUMN)
-                        val indexAccessible =
-                            cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARD_ACCESSIBLE_COLUMN)
+        }
 
-                        hoards.add(
-                            Hoard(
-                                id = cursor.getLong(indexId),
-                                hoardName = cursor.getString(indexName),
-                                isHoardAccessible = cursor.getInt(indexAccessible) != 0,
-                                goldHoarded = cursor.getInt(indexGoldHoarded)
-                            )
+        override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+            data?.use { cursor ->
+                val hoards = arrayListOf<Hoard>()
+                while (cursor.moveToNext()) {
+                    val indexId = cursor.getColumnIndex(HoardContract.KEY_ID)
+                    val indexName =
+                        cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARD_NAME_COLUMN)
+                    val indexGoldHoarded =
+                        cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARDED_COLUMN)
+                    val indexAccessible =
+                        cursor.getColumnIndex(HoardContract.KEY_GOLD_HOARD_ACCESSIBLE_COLUMN)
+
+                    hoards.add(
+                        Hoard(
+                            id = cursor.getLong(indexId),
+                            hoardName = cursor.getString(indexName),
+                            isHoardAccessible = cursor.getInt(indexAccessible) != 0,
+                            goldHoarded = cursor.getInt(indexGoldHoarded)
                         )
-                    }
-
-                    hoardsAdapter.submitList(hoards)
+                    )
                 }
+
+                hoardsAdapter.submitList(hoards)
             }
         }
+
+        override fun onLoaderReset(loader: Loader<Cursor>) {}
+    }
+
+    companion object {
+        private const val LOADER_ID = 123
     }
 }
